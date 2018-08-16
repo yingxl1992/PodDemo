dc32c0774604aca1fc7fe07f2d030e6418e634c8
上海-应晓立 <yingxiaoli@jd.com>
2018-08-16 14:19:52 +0800

    first commit

---
+++ Classes/YXLObject.h
@@ -0,0 +1,12 @@
+//
+//  YXLObject.h
+//  FBSnapshotTestCase
+//
+//  Created by 应晓立 on 2018/8/16.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YXLObject : NSObject
+
+@end
---
+++ Classes/YXLObject.m
@@ -0,0 +1,12 @@
+//
+//  YXLObject.m
+//  FBSnapshotTestCase
+//
+//  Created by 应晓立 on 2018/8/16.
+//
+
+#import "YXLObject.h"
+
+@implementation YXLObject
+
+@end
